9af40d7d47019c1c346d2c2646dbb7603fb2b92d
takuya3211 <takuya@pioncloud.com>
2015-03-31 08:05:51 +0900

    Initial Commit

---
+++ UsserPyon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UsserPyon
+//
+//  Created by 伊藤 拓也 on 2015/03/31.
+//  Copyright (c) 2015年 Takuya Ito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
